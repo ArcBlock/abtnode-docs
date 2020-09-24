9727e1ad68b4b4d053a69d8d12c7411bbd39cc61
linchen1987 <linchen.1987@foxmail.com>
2020-09-24 14:22:20 +0800

    fix: exec travis command failed without --pro (#55)

--- .makefiles/trigger_main_build.sh
+++ .makefiles/trigger_main_build.sh
@@ -6,9 +6,9 @@ if [ "$TRAVIS_PULL_REQUEST" = false ]; then
 
   travis login --pro --github-token=$GITHUB_TOKEN
 
-  BUILD_ID=`travis branches --repo $MAIN_REPO_SLUG | grep master | awk '{print $2}' | awk -F# '{print $2}'`
+  BUILD_ID=`travis branches --pro --repo $MAIN_REPO_SLUG | grep master | awk '{print $2}' | awk -F# '{print $2}'`
   echo "Last build for main repo is: $BUILD_ID"
 
-  travis cancel --repo $MAIN_REPO_SLUG $BUILD_ID
-  travis restart --repo $MAIN_REPO_SLUG $BUILD_ID
+  travis cancel --pro --repo $MAIN_REPO_SLUG $BUILD_ID
+  travis restart --pro --repo $MAIN_REPO_SLUG $BUILD_ID
 fi
